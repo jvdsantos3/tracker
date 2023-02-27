e2f6cdfec2703ce6aee10d8fd99841552843129e
João <jv.dossantos012@gmail.com>
2023-02-27 18:27:52 -0300

    Implementação do formulario e do cronometro de tarefas

---
+++ src/components/Formulario.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+      </div>
+
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
+
+export default defineComponent({
+  name: "FormularioTarefa",
+
+  components: {
+    Cronometro,
+  },
+
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+
+    finalizar() {
+      clearInterval(this.cronometro);
+    }
+  },
+
+})
+
+</script>
