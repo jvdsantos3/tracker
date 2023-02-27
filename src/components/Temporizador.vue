32e3b97168b24e18ddd38580b9905eb9fd67f63c
João <jv.dossantos012@gmail.com>
2023-02-27 19:42:11 -0300

    Implementação do temporizador

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+
+    <button class="button" @click="iniciar" :disabled="cronometoRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!cronometoRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
+
+export default defineComponent({
+  name: 'TemporizadorTarefa',
+
+  components: {
+    Cronometro,
+  },
+
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometoRodando: false
+    };
+  },
+
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+
+      this.cronometoRodando = true
+    },
+
+    finalizar() {
+      clearInterval(this.cronometro);
+
+      this.cronometoRodando = false
+    }
+  },
+})
+
+</script>
